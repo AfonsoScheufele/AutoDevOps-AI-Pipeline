4c2391091f12df6f1e2e85ff529200492a7d9513
Afonso <martinischeufeleafonso@gmail.com>
2025-08-11 21:28:19 -0300

    Adiciona testes básicos com Jest

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ const http = require('http');
 const hostname = '0.0.0.0';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
+const requestHandler = (req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World from AutoDevOps!\n');
-});
+};
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+const server = http.createServer(requestHandler);
+
+if (require.main === module) {
+  server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+  });
+}
+
+module.exports = server;
---
+++ tests/app.test.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const app = require('../app'); // vamos ajustar seu app para exportar o servidor
+
+let server;
+
+beforeAll(() => {
+  server = app.listen(3001);
+});
+
+afterAll(() => {
+  server.close();
+});
+
+test('GET / retorna "Hello World from AutoDevOps!"', (done) => {
+  http.get('http://localhost:3001', (res) => {
+    let data = '';
+
+    res.on('data', chunk => data += chunk);
+
+    res.on('end', () => {
+      expect(data).toBe('Hello World from AutoDevOps!\n');
+      done();
+    });
+  });
+});
